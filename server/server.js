8a326425ad01d95ae69e34105040b650266f8012
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-02 23:10:24 -1000

    integrate mqtt using public hivemq broker

--- server/server.js
+++ server/server.js
@@ -5,16 +5,72 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 
+const mqtt = require("mqtt");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Kilowatt server");
 });
 
+// MQTT Connection using HiveMQ Public Broker
+const protocol = "mqtt";
+const host = "broker.emqx.io";
+const port = "1883";
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+
+const connectUrl = `${protocol}://${host}:${port}`;
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: "emqx",
+  password: "public",
+  reconnectPeriod: 1000,
+});
+
+const acTopic = "/hawaiihac/ac";
+const refrigetorTopic = "/hawaiihac/refrigerator";
+const ceilingFanTopic = "/hawaiihac/ceilingFan";
+const ovenTopic = "/hawaiihac/oven";
+const tvTopic = "/hawaiihac/tv";
+const washerDryerTopic = "/hawaiihac/tv";
+const porchLightTopic = "/hawaiihac/porchLight";
+const ceilingLightTopic = "/hawaiihac/ceilingLight";
+
+client.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  client.subscribe(acTopic, () => {
+    console.log(`Subscribed to topic ${acTopic}`);
+  });
+  client.subscribe(refrigetorTopic, () => {
+    console.log(`Subscribed to topic ${refrigetorTopic}`);
+  });
+  client.subscribe(ceilingFanTopic, () => {
+    console.log(`Subscribed to topic ${ceilingFanTopic}`);
+  });
+  client.subscribe(ovenTopic, () => {
+    console.log(`Subscribed to topic ${ovenTopic}`);
+  });
+  client.subscribe(tvTopic, () => {
+    console.log(`Subscribed to topic ${tvTopic}`);
+  });
+  client.subscribe(washerDryerTopic, () => {
+    console.log(`Subscribed to topic ${washerDryerTopic}`);
+  });
+  client.subscribe(porchLightTopic, () => {
+    console.log(`Subscribed to topic ${porchLightTopic}`);
+  });
+  client.subscribe(ceilingLightTopic, () => {
+    console.log(`Subscribed to topic ${ceilingLightTopic}`);
+  });
+});
+
 // WebsocketIO connection
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   },
@@ -27,11 +83,16 @@ io.on("connection", (socket) => {
   // to receive messages, use socket.on
   // to send messages, use socket.emit
   // Navigate to client/src/app/page.tsx to view where the message is received
-  io.emit("ac unit", { message: "this is the ac unit" });
 
   socket.on("disconnect", () => {
     console.log("Websocket client disconnected");
   });
+});
+
+client.on("message", async (acTopic, paylod) => {
+  const data = JSON.parse(paylod.toString());
+  io.emit("ac unit", data);
+  console.log(data);
 });
 
 const PORT = process.env.PORT || 3001;
