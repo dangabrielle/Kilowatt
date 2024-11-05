f29290933a778a6948daf1c53328aa2aa0e9248b
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-04 14:28:10 -1000

    add svg wave animation

--- client/src/app/components/ProgressBar.tsx
+++ client/src/app/components/ProgressBar.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 
 interface ProgressBarProps {
   status: boolean;
@@ -26,7 +25,7 @@ const ProgressBar = ({ status, monthlyKWh }: ProgressBarProps) => {
     const intervalId = setInterval(() => {
       setProgress((prev) => {
         const newProgress = Math.min(prev + energyRatePerSecond, dailykWh);
-        console.log("Progress:", newProgress); // Log the new progress value
+        console.log("Progress:", newProgress);
         return newProgress;
       });
     }, 1000);
@@ -34,17 +33,42 @@ const ProgressBar = ({ status, monthlyKWh }: ProgressBarProps) => {
   }, [status]);
 
   const heightPercentage = (progress / dailykWh) * 100;
-  console.log("height percentage", heightPercentage);
-  //   const barHeight = Math.max((progress / monthlyKWh) * 100, 1);
+
   return (
     <div className="relative w-3/4 h-full  bg-gray-200 rounded-2xl overflow-hidden">
-      <motion.div
-        className="absolute bottom-0 w-full h-full z-0 bg-green-500"
-        style={{ height: `${heightPercentage}%` }}
-        initial={{ height: 0 }}
-        animate={{ height: `${heightPercentage}%` }}
-      ></motion.div>
-      <div className="absolute inset-0 flex items-center justify-center z-10">
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        style={{ display: "none" }}
+      >
+        <symbol id="wave">
+          <path d="M420,20c21.5-0.4,38.8-2.5,51.1-4.5c13.4-2.2,26.5-5.2,27.3-5.4C514,6.5,518,4.7,528.5,2.7c7.1-1.3,17.9-2.8,31.5-2.7c0,0,0,0,0,0v20H420z"></path>
+          <path d="M420,20c-21.5-0.4-38.8-2.5-51.1-4.5c-13.4-2.2-26.5-5.2-27.3-5.4C326,6.5,322,4.7,311.5,2.7C304.3,1.4,293.6-0.1,280,0c0,0,0,0,0,0v20H420z"></path>
+          <path d="M140,20c21.5-0.4,38.8-2.5,51.1-4.5c13.4-2.2,26.5-5.2,27.3-5.4C234,6.5,238,4.7,248.5,2.7c7.1-1.3,17.9-2.8,31.5-2.7c0,0,0,0,0,0v20H140z"></path>
+          <path d="M140,20c-21.5-0.4-38.8-2.5-51.1-4.5c-13.4-2.2-26.5-5.2-27.3-5.4C46,6.5,42,4.7,31.5,2.7C24.3,1.4,13.6-0.1,0,0c0,0,0,0,0,0l0,20H140z"></path>
+        </symbol>
+      </svg>
+      <div className="box">
+        <div
+          id="water"
+          className="water absolute bottom-0 w-full h-full"
+          style={{
+            transform: `translateY(${100 - heightPercentage}%)`,
+            transition: "transform 0.06s ease-in-out",
+          }}
+        >
+          <svg viewBox="0 0 560 20" className="water_wave water_wave_back">
+            <use xlinkHref="#wave"></use>
+          </svg>
+          <svg viewBox="0 0 560 20" className="water_wave water_wave_front">
+            <use xlinkHref="#wave"></use>
+          </svg>
+        </div>
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center z-10 text-center">
         {progress.toFixed(2)} kWh
       </div>
     </div>
