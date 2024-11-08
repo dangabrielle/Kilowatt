2ae9331a644d3c91cb4ed7b9f86a023c17cb1456
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-07 22:42:35 -1000

    wip: auth page

--- client/src/app/auth/page.tsx
+++ client/src/app/auth/page.tsx
@@ -1,18 +1,43 @@
+"use client";
+
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Volcano from "../models/Volcano";
+import Sky from "../models/Sky";
 
 const login = () => {
   return (
     <>
-      <div className="p-30">
-        <h1 className="relative text-white z-50 bg-green-800 p-2 pr-3 pl-3 bg-opacity-70 shadow-gray-700 shadow-md ml-2 text-center text-xs md:text-md lg:text-base rounded-md">
-          <b>HAWAI'I HACKATHON 2024</b> <br /> Hawai'i Keiki Museum Challenge
-        </h1>
-
-        <h1 className="relative text-stone-800 p-2 bg-opacity-70 mt-2 text-center text-xs md:text-md lg:text-base">
-          <em>KILOWATT</em>
-        </h1>
-        <div className="relative  text-white z-50 bg-gray-400  shadow-gray-700 shadow-md hover:bg-slate-500 bg-opacity-70 rounded-md p-2 text-xs md:text-md lg:text-base">
-          <a href="/api/auth/login">Login</a>
+      <div className="h-screen w-full flex flex-col items-center">
+        <div className="absolute z-10 w-1/3 h-2/5 pt-16">
+          <div className="relative flex flex-col items-center z-50 text-center sm:text-s md:text-base lg:text-lg xl:text-xl 2xl:text-2xl floatText">
+            <h1>HAWAI'I HACKATHON 2024</h1>
+            <h1>Hawai'i Keiki Museum Challenge</h1>
+            <h1 className="relative text-stone-800 p-2 bg-opacity-70 mt-2 text-center text-xs md:text-md lg:text-base">
+              <em>KILOWATT: ISLAND SURGE</em>
+            </h1>
+            <div className="relative text-center text-white z-50 bg-gray-400 w-1/2 shadow-gray-700 shadow-md hover:bg-slate-500 bg-opacity-70 rounded-md p-2 text-xs md:text-md lg:text-base">
+              <a href="/api/auth/login">Login of Sign Up</a>
+            </div>
+          </div>
+        </div>
+        <div className="h-screen w-screen z-0">
+          <Canvas
+            camera={{ position: [-60, 10, 80], fov: 50 }}
+            // style={{ width: "100%", height: "100%" }}
+          >
+            <Suspense fallback={null}>
+              <ambientLight intensity={2} />
+              <group position={[0, -10, 0]} scale={0.285}>
+                <Sky />
+              </group>
+              <group position={[0, -20, 0]} scale={0.7}>
+                <directionalLight position={[5, 10, 5]} intensity={2} />
+                <Volcano cumulativePercentage={0} />
+              </group>
+            </Suspense>
+          </Canvas>
         </div>
       </div>
     </>
