83906c0aed5047df52267974b6e501cb58728f8b
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-07 20:58:07 -1000

    add new prop in ts types

--- client/types.tsx
+++ client/types.tsx
@@ -2,6 +2,7 @@ export type ProgressBarProps = {
   status: boolean;
   monthlyKWh: number;
   onPercentageChange: (percentage: number) => void;
+  onNewText: (percentage: number) => void;
 };
 
 export type ApplianceSceneProps = {
