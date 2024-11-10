7f8a87df5abefd658defcfc40d2ce55d4dea4960
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-09 14:00:37 -1000

    convert to ts

---
+++ client/src/socket.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { io } from "socket.io-client";
+
+console.log(
+  "connecting to socket.io server at:",
+  process.env.NEXT_PUBLIC_WEBSOCKET_SERVER_URL
+);
+
+export const socket = io(process.env.NEXT_PUBLIC_WEBSOCKET_SERVER_URL, {
+  transports: ["websocket"],
+});
