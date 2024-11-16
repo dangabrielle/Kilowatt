9d6c2a6ad396ddc459dcfa0de3388a4c13b7e6a3
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-15 18:15:50 -1000

    redirect if not logged in

--- client/src/app/island/page.tsx
+++ client/src/app/island/page.tsx
@@ -171,242 +171,257 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-screen w-screen flex flex-row justify-evenly bg-slate-200">
-        <div className="w-2/5 flex h-full flex-row items-center justify-evenly gap-x-5 p-10 pl-7">
-          <div className="w-1/2 h-full flex flex-col justify-evenly">
-            <div
-              className={`w-full h-full m-2 pl-5 ${
-                applianceStatus.ac ? colorPanel(percentage.ac) : "bg-slate-300"
-              } rounded-3xl`}
-            >
-              <AirConditionerScene
-                status={applianceStatus.ac}
-                monthlyKWh={energyConsumedPerMonth.ac}
-                onPercentageChange={(newPercentage) => {
-                  handlePercentageChange("ac", newPercentage);
-                }}
-              />
-            </div>
-            <div
-              className={`w-full h-full m-2 pl-5 ${
-                applianceStatus.refrigerator
-                  ? colorPanel(percentage.refrigerator)
-                  : "bg-slate-300"
-              }  rounded-3xl`}
-            >
-              <RefrigeratorScene
-                status={applianceStatus.refrigerator}
-                monthlyKWh={energyConsumedPerMonth.refrigerator}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("refrigerator", newPercentage)
-                }
-              />
-            </div>
-            <div
-              className={`m-2 w-full h-full pl-5 ${
-                applianceStatus.ceilingFan
-                  ? colorPanel(percentage.ceilingFan)
-                  : "bg-slate-300"
-              }  rounded-3xl`}
-            >
-              <CeilingFanScene
-                status={applianceStatus.ceilingFan}
-                monthlyKWh={energyConsumedPerMonth.ceilingFan}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("ceilingFan", newPercentage)
-                }
-              />
-            </div>
-
-            <div
-              className={`m-2 pl-5  w-full h-full ${
-                applianceStatus.oven
-                  ? colorPanel(percentage.oven)
-                  : "bg-slate-300"
-              } rounded-3xl`}
-            >
-              <ElectricOvenScene
-                status={applianceStatus.oven}
-                monthlyKWh={energyConsumedPerMonth.oven}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("oven", newPercentage)
-                }
-              />
-            </div>
-          </div>
-          <div className="w-1/2 h-full flex flex-col justify-evenly">
-            <div
-              className={`m-2 pl-5 w-full h-full ${
-                applianceStatus.tv ? colorPanel(percentage.tv) : "bg-slate-300"
-              }  rounded-3xl`}
-            >
-              <TelevisionScene
-                status={applianceStatus.tv}
-                monthlyKWh={energyConsumedPerMonth.tv}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("tv", newPercentage)
-                }
-              />
-            </div>
-            <div
-              className={`m-2 pl-5 w-full h-full ${
-                applianceStatus.washerDryer
-                  ? colorPanel(percentage.washerDryer)
-                  : "bg-slate-300"
-              }  rounded-3xl`}
-            >
-              <WasherDryerScene
-                status={applianceStatus.washerDryer}
-                monthlyKWh={energyConsumedPerMonth.washerDryer}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("washerDryer", newPercentage)
-                }
-              />
-            </div>
-            <div
-              className={`m-2 pl-5 w-full h-full ${
-                applianceStatus.porchLight
-                  ? colorPanel(percentage.porchLight)
-                  : "bg-slate-300"
-              } rounded-3xl`}
-            >
-              <PorchLightScene
-                status={applianceStatus.porchLight}
-                monthlyKWh={energyConsumedPerMonth.porchLight}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("porchLight", newPercentage)
-                }
-              />
-            </div>
-            <div
-              className={`m-2 pl-5 w-full h-full ${
-                applianceStatus.ceilingLight
-                  ? colorPanel(percentage.ceilingLight)
-                  : "bg-slate-300"
-              } rounded-3xl`}
-            >
-              <CeilingLightScene
-                status={applianceStatus.ceilingLight}
-                monthlyKWh={energyConsumedPerMonth.ceilingLight}
-                onPercentageChange={(newPercentage) =>
-                  handlePercentageChange("ceilingLight", newPercentage)
-                }
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="relative w-3/5 h-screen">
-          <div className="absolute top-5 flex flex-row right-10 items-center z-50">
-            <div className="mr-5 text-amber-950">
-              {user && `Aloha, ${user.nickname}!`}
-            </div>
-            <button
-              onClick={handlePlay}
-              className={`bg-neutral-200 shadow-md mr-5 hover:scale-105 hover:bg-emerald-300 hover:bg-opacity-70 active:scale-110 active:bg-emerald-400 shadow-gray-500 ${
-                isPlaying ? "bg-emerald-300 bg-opacity-80" : "bg-opacity-10"
-              } p-1 pr-3 pl-3 rounded-2xl`}
-            >
-              🎵
-            </button>
-            <audio ref={audioPlayback} src="/island_bgmusic.mp3" loop />
-            <audio ref={sinkAudioRef} src="/whistle_down.mp3" />
-            <button className=" bg-neutral-200 shadow-md  hover:scale-105 hover:bg-orange-200 hover:bg-opacity-70 active:scale-110 active:bg-orange-200 shadow-gray-500 text-rose-950 bg-opacity-80 p-1 pr-3 pl-3 rounded-2xl">
-              <a href="/api/auth/logout">Logout</a>
-            </button>
-          </div>
-          <div className="absolute flex w-full h-1/3 flex-row z-10 p-10 justify-center">
-            <div
-              style={{
-                textAlign: "center",
-                position: "relative",
-
-                marginRight: "1rem",
-                color: "white",
-                height: "50%",
-                width: "50%",
-                fontSize: "1.5rem",
-                textShadow: "black 2px 2px 2px",
-              }}
-            >
-              <div className="relative flex flex-col items-center -ml-28">
-                <h1 className="relative text-center">ISLAND ENERGY USAGE</h1>
-
-                <GaugeComponent
-                  style={{
-                    position: "relative",
-                    width: "70%",
-                    opacity: 90,
+      {user ? (
+        <div className="h-screen w-screen flex flex-row justify-evenly bg-slate-200">
+          <div className="w-2/5 flex h-full flex-row items-center justify-evenly gap-x-5 p-10 pl-7">
+            <div className="w-1/2 h-full flex flex-col justify-evenly">
+              <div
+                className={`w-full h-full m-2 pl-5 ${
+                  applianceStatus.ac
+                    ? colorPanel(percentage.ac)
+                    : "bg-slate-300"
+                } rounded-3xl`}
+              >
+                <AirConditionerScene
+                  status={applianceStatus.ac}
+                  monthlyKWh={energyConsumedPerMonth.ac}
+                  onPercentageChange={(newPercentage) => {
+                    handlePercentageChange("ac", newPercentage);
                   }}
-                  marginInPercent={{
-                    top: 0.12,
-                    bottom: 0.0,
-                    left: 0.15,
-                    right: 0.1,
-                  }}
-                  arc={{
-                    emptyColor: "#a4c6b7c7",
+                />
+              </div>
+              <div
+                className={`w-full h-full m-2 pl-5 ${
+                  applianceStatus.refrigerator
+                    ? colorPanel(percentage.refrigerator)
+                    : "bg-slate-300"
+                }  rounded-3xl`}
+              >
+                <RefrigeratorScene
+                  status={applianceStatus.refrigerator}
+                  monthlyKWh={energyConsumedPerMonth.refrigerator}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("refrigerator", newPercentage)
+                  }
+                />
+              </div>
+              <div
+                className={`m-2 w-full h-full pl-5 ${
+                  applianceStatus.ceilingFan
+                    ? colorPanel(percentage.ceilingFan)
+                    : "bg-slate-300"
+                }  rounded-3xl`}
+              >
+                <CeilingFanScene
+                  status={applianceStatus.ceilingFan}
+                  monthlyKWh={energyConsumedPerMonth.ceilingFan}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("ceilingFan", newPercentage)
+                  }
+                />
+              </div>
 
-                    subArcs: [
-                      {
-                        limit: 50,
-                        color: "#5BE12C",
-                        showTick: true,
-                      },
-                      {
-                        limit: 85,
-                        color: "#F58B19",
-                        showTick: true,
-                      },
-                      {
-                        limit: 100,
-                        color: "#EA4228",
-                        showTick: true,
-                      },
-                    ],
-                  }}
-                  labels={{
-                    tickLabels: {
-                      defaultTickValueConfig: {
-                        style: {
-                          fill: "white",
-                          fontSize: "1rem",
-                          textShadow: "black 1px 1px 1px",
-                        },
-                      },
-                    },
-                  }}
-                  value={cumulativePercentage}
+              <div
+                className={`m-2 pl-5  w-full h-full ${
+                  applianceStatus.oven
+                    ? colorPanel(percentage.oven)
+                    : "bg-slate-300"
+                } rounded-3xl`}
+              >
+                <ElectricOvenScene
+                  status={applianceStatus.oven}
+                  monthlyKWh={energyConsumedPerMonth.oven}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("oven", newPercentage)
+                  }
                 />
               </div>
             </div>
-            <div className="w-2/5 h-1/2 mt-14 mr-10 -ml-16 flex flex-row flex-1 items-center justify-center floatText">
-              <p className="sm:text-xs md:text-base lg:text-lg 2xl:text-2xl break-words overflow-hidden max-w-full text-center">
-                {textBubble}
-              </p>
+            <div className="w-1/2 h-full flex flex-col justify-evenly">
+              <div
+                className={`m-2 pl-5 w-full h-full ${
+                  applianceStatus.tv
+                    ? colorPanel(percentage.tv)
+                    : "bg-slate-300"
+                }  rounded-3xl`}
+              >
+                <TelevisionScene
+                  status={applianceStatus.tv}
+                  monthlyKWh={energyConsumedPerMonth.tv}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("tv", newPercentage)
+                  }
+                />
+              </div>
+              <div
+                className={`m-2 pl-5 w-full h-full ${
+                  applianceStatus.washerDryer
+                    ? colorPanel(percentage.washerDryer)
+                    : "bg-slate-300"
+                }  rounded-3xl`}
+              >
+                <WasherDryerScene
+                  status={applianceStatus.washerDryer}
+                  monthlyKWh={energyConsumedPerMonth.washerDryer}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("washerDryer", newPercentage)
+                  }
+                />
+              </div>
+              <div
+                className={`m-2 pl-5 w-full h-full ${
+                  applianceStatus.porchLight
+                    ? colorPanel(percentage.porchLight)
+                    : "bg-slate-300"
+                } rounded-3xl`}
+              >
+                <PorchLightScene
+                  status={applianceStatus.porchLight}
+                  monthlyKWh={energyConsumedPerMonth.porchLight}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("porchLight", newPercentage)
+                  }
+                />
+              </div>
+              <div
+                className={`m-2 pl-5 w-full h-full ${
+                  applianceStatus.ceilingLight
+                    ? colorPanel(percentage.ceilingLight)
+                    : "bg-slate-300"
+                } rounded-3xl`}
+              >
+                <CeilingLightScene
+                  status={applianceStatus.ceilingLight}
+                  monthlyKWh={energyConsumedPerMonth.ceilingLight}
+                  onPercentageChange={(newPercentage) =>
+                    handlePercentageChange("ceilingLight", newPercentage)
+                  }
+                />
+              </div>
             </div>
           </div>
-          <div className="h-full w-full z-0">
-            <Canvas
-              camera={{ position: [-60, 10, 80], fov: 50 }}
-              style={{ width: "100%", height: "100%" }}
-            >
-              <Suspense fallback={null}>
-                <ambientLight intensity={2} />
-                <group position={[0, -10, 0]} scale={0.25}>
-                  <Sky />
-                </group>
-                <group position={[0, -18, 0]} scale={0.7}>
-                  <directionalLight position={[5, 10, 5]} intensity={2} />
-                  <Volcano cumulativePercentage={cumulativePercentage} />
-                </group>
-                <OrbitControls />
-              </Suspense>
-            </Canvas>
+
+          <div className="relative w-3/5 h-screen">
+            <div className="absolute top-5 flex flex-row right-10 items-center z-50">
+              <div className="mr-5 text-amber-950">
+                {user && `Aloha, ${user.nickname}!`}
+              </div>
+              <button
+                onClick={handlePlay}
+                className={`bg-neutral-200 shadow-md mr-5 hover:scale-105 hover:bg-emerald-300 hover:bg-opacity-70 active:scale-110 active:bg-emerald-400 shadow-gray-500 ${
+                  isPlaying ? "bg-emerald-300 bg-opacity-80" : "bg-opacity-10"
+                } p-1 pr-3 pl-3 rounded-2xl`}
+              >
+                🎵
+              </button>
+              <audio ref={audioPlayback} src="/island_bgmusic.mp3" loop />
+              <audio ref={sinkAudioRef} src="/whistle_down.mp3" />
+              <button className=" bg-neutral-200 shadow-md  hover:scale-105 hover:bg-orange-200 hover:bg-opacity-70 active:scale-110 active:bg-orange-200 shadow-gray-500 text-rose-950 bg-opacity-80 p-1 pr-3 pl-3 rounded-2xl">
+                <a href="/api/auth/logout">Logout</a>
+              </button>
+            </div>
+            <div className="absolute flex w-full h-1/3 flex-row z-10 p-10 justify-center">
+              <div
+                style={{
+                  textAlign: "center",
+                  position: "relative",
+
+                  marginRight: "1rem",
+                  color: "white",
+                  height: "50%",
+                  width: "50%",
+                  fontSize: "1.5rem",
+                  textShadow: "black 2px 2px 2px",
+                }}
+              >
+                <div className="relative flex flex-col items-center -ml-28">
+                  <h1 className="relative text-center">ISLAND ENERGY USAGE</h1>
+
+                  <GaugeComponent
+                    style={{
+                      position: "relative",
+                      width: "70%",
+                      opacity: 90,
+                    }}
+                    marginInPercent={{
+                      top: 0.12,
+                      bottom: 0.0,
+                      left: 0.15,
+                      right: 0.1,
+                    }}
+                    arc={{
+                      emptyColor: "#a4c6b7c7",
+
+                      subArcs: [
+                        {
+                          limit: 50,
+                          color: "#5BE12C",
+                          showTick: true,
+                        },
+                        {
+                          limit: 85,
+                          color: "#F58B19",
+                          showTick: true,
+                        },
+                        {
+                          limit: 100,
+                          color: "#EA4228",
+                          showTick: true,
+                        },
+                      ],
+                    }}
+                    labels={{
+                      tickLabels: {
+                        defaultTickValueConfig: {
+                          style: {
+                            fill: "white",
+                            fontSize: "1rem",
+                            textShadow: "black 1px 1px 1px",
+                          },
+                        },
+                      },
+                    }}
+                    value={cumulativePercentage}
+                  />
+                </div>
+              </div>
+              <div className="w-2/5 h-1/2 mt-14 mr-10 -ml-16 flex flex-row flex-1 items-center justify-center floatText">
+                <p className="sm:text-xs md:text-base lg:text-lg 2xl:text-2xl break-words overflow-hidden max-w-full text-center">
+                  {textBubble}
+                </p>
+              </div>
+            </div>
+            <div className="h-full w-full z-0">
+              <Canvas
+                camera={{ position: [-60, 10, 80], fov: 50 }}
+                style={{ width: "100%", height: "100%" }}
+              >
+                <Suspense fallback={null}>
+                  <ambientLight intensity={2} />
+                  <group position={[0, -10, 0]} scale={0.25}>
+                    <Sky />
+                  </group>
+                  <group position={[0, -18, 0]} scale={0.7}>
+                    <directionalLight position={[5, 10, 5]} intensity={2} />
+                    <Volcano cumulativePercentage={cumulativePercentage} />
+                  </group>
+                  <OrbitControls />
+                </Suspense>
+              </Canvas>
+            </div>
           </div>
+          <Loader />
         </div>
-        <Loader />
-      </div>
+      ) : (
+        <div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-300 bg-opacity-30">
+          <h1 className="text-3xl">
+            Oops! Please login or sign up to access Kilowatt Island Surge.
+          </h1>
+          <button className="m-8 bg-neutral-200 shadow-md  hover:scale-105 hover:bg-orange-200 hover:bg-opacity-70 active:scale-110 active:bg-orange-200 shadow-gray-500 text-rose-950 bg-opacity-80 p-3 pr-4 pl-4 rounded-2xl">
+            <a href="/api/auth/login">Login or Sign Up Here</a>
+          </button>
+        </div>
+      )}
     </>
   );
 }
